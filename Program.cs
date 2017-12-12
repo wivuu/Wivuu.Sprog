9068644dd3945406e3120171b97068fea1e15381
Erik O'Leary <erikoleary@Onionbook.local>
2017-12-11 22:32:10 -0600

    Non failing cases

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Linq;
 
 namespace csparser
@@ -6,32 +7,67 @@ namespace csparser
     using static Fundamentals;
     static class Fundamentals
     {
-        public static bool Whitespace(char c) =>
-            c == ' ' || c == '\t' || c == '\r' || c == '\n';
-            
-        public static bool AThroughZ(char c) => 
-            ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z');
-            
+        static int MatchWhile(this ReadOnlySpan<char> input, Func<char, bool> predicate, bool failOnMiss)
+        {
+            var i = 0;
+            while (i < input.Length && predicate( input[i] ))
+                ++i;
+
+            if (failOnMiss && i == 0)
+                throw new Exception($"Unable to parse: \"{input.AsString()}\"");
+
+            return i;
+        }
+        
+        static int MatchWhile(this ReadOnlySpan<char> input, Func<char, bool> predicate, bool failOnMiss, int take)
+        {
+            var i = 0;
+            while (i < input.Length && i < take && predicate( input[i] ))
+                ++i;
+
+            if (failOnMiss && i == 0)
+                throw new Exception("Unable to parse");
+
+            return i;
+        }
+
+        public static ReadOnlySpan<char> TakeOne(
+            this ReadOnlySpan<char> input, Func<char, bool> predicate, out char match)
+        {
+            var i = MatchWhile(input, predicate, failOnMiss: true, take: 1);
+            match = input[0];
+
+            return input.Slice(i);
+        }
+
         public static ReadOnlySpan<char> Take(
             this ReadOnlySpan<char> input, Func<char, bool> predicate, out ReadOnlySpan<char> match)
         {
-            var matchIndex = 0;
-            
-            while (matchIndex < input.Length && predicate( input[matchIndex] ))
-                ++matchIndex;
+            var i = MatchWhile(input, predicate, failOnMiss: true);
+            match = input.Slice(0, i);
 
-            match = matchIndex > 0
-                ? input.Slice(0, matchIndex)
-                : throw new Exception("Unable to parse");
-
-            return input.Slice(matchIndex);
+            return input.Slice(i);
         }
 
+        public static ReadOnlySpan<char> SkipOne(this ReadOnlySpan<char> input, Func<char, bool> predicate) =>
+            input.Slice(MatchWhile(input, predicate, failOnMiss: false, take: 1));
+
         public static ReadOnlySpan<char> Skip(this ReadOnlySpan<char> input, Func<char, bool> predicate) =>
-            Take(input, predicate, out _);
+            input.Slice(MatchWhile(input, predicate, failOnMiss: false));
 
         public static string AsString(this ReadOnlySpan<char> input) =>
             new String(input.ToArray());
+            
+        public static string Concat(this ReadOnlySpan<char> input, params ReadOnlySpan<char>[] rest)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append(input.AsString());
+            for (var i = 0; i < rest.Length; ++i)
+                sb.Append(rest[i].AsString());
+
+            return sb.ToString();
+        }
     }
 
     class Program
@@ -39,13 +75,15 @@ namespace csparser
         static void Main(string[] args)
         {
             var remaining = 
-                @"  This is some string  "
+                @"1  B3his is some string  "
                 .AsSpan()
-                .Skip(Whitespace)
-                .Take(AThroughZ, out var word)
-                .Skip(Whitespace);
+                .SkipOne(Char.IsDigit)
+                .Skip(Char.IsWhiteSpace)
+                .TakeOne(Char.IsLetter, out var prefix)
+                .Take(Char.IsLetterOrDigit, out var word)
+                .Skip(Char.IsWhiteSpace);
 
-            Console.WriteLine($"Word: {word.AsString()}");
+            Console.WriteLine($"     Word: {prefix}{word.AsString()}");
             Console.WriteLine($"Remaining: {remaining.AsString()}");
         }
     }
