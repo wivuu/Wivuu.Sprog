231387f3482f835c5fac5649671c8c3d78e26ea7
Erik O'Leary <erikoleary@Onionbook.local>
2017-12-11 22:58:58 -0600

    Split code into two files

---
+++ Parser.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Text;
+using System.Linq;
+
+namespace csparser
+{
+    static partial class Parser
+    {
+        static int MatchWhile(this ReadOnlySpan<char> input, Func<char, bool> predicate)
+        {
+            var i = 0;
+            while (i < input.Length && predicate( input[i] ))
+                ++i;
+
+            return i;
+        }
+        
+        static int MatchWhile(this ReadOnlySpan<char> input, Func<char, bool> predicate, int take)
+        {
+            var i = 0;
+            while (i < input.Length && i < take && predicate( input[i] ))
+                ++i;
+
+            return i;
+        }
+
+        public static ReadOnlySpan<char> TakeOne(
+            this ReadOnlySpan<char> input, Func<char, bool> predicate, out char match)
+        {
+            var i = MatchWhile(input, predicate, take: 1);
+            match = input[0];
+
+            return input.Slice(i);
+        }
+
+        public static ReadOnlySpan<char> Take(
+            this ReadOnlySpan<char> input, Func<char, bool> predicate, out ReadOnlySpan<char> match)
+        {
+            var i = MatchWhile(input, predicate);
+            match = input.Slice(0, i);
+
+            return input.Slice(i);
+        }
+
+        public static ReadOnlySpan<char> SkipOne(this ReadOnlySpan<char> input, Func<char, bool> predicate) =>
+            input.Slice(MatchWhile(input, predicate, take: 1));
+
+        public static ReadOnlySpan<char> Skip(this ReadOnlySpan<char> input, Func<char, bool> predicate) =>
+            input.Slice(MatchWhile(input, predicate));
+
+        public static string AsString(this ReadOnlySpan<char> input) =>
+            new String(input.ToArray());
+            
+        public static string Concat(this ReadOnlySpan<char> input, params ReadOnlySpan<char>[] rest)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append(input.AsString());
+            for (var i = 0; i < rest.Length; ++i)
+                sb.Append(rest[i].AsString());
+
+            return sb.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,86 +4,33 @@ using System.Linq;
 
 namespace csparser
 {
-    using static Fundamentals;
-    static class Fundamentals
+    using static Parser;
+    using static Char;
+
+    static class Program
     {
-        static int MatchWhile(this ReadOnlySpan<char> input, Func<char, bool> predicate, bool failOnMiss)
+        static ReadOnlySpan<char> ParseIdentifier(this ReadOnlySpan<char> input, out string identifier)
         {
-            var i = 0;
-            while (i < input.Length && predicate( input[i] ))
-                ++i;
+            var result = input
+                 .TakeOne(IsLetter, out var prefix)
+                 .Take(IsLetterOrDigit, out var word)
+                 .Skip(IsWhiteSpace);
 
-            if (failOnMiss && i == 0)
-                throw new Exception($"Unable to parse: \"{input.AsString()}\"");
-
-            return i;
-        }
-        
-        static int MatchWhile(this ReadOnlySpan<char> input, Func<char, bool> predicate, bool failOnMiss, int take)
-        {
-            var i = 0;
-            while (i < input.Length && i < take && predicate( input[i] ))
-                ++i;
-
-            if (failOnMiss && i == 0)
-                throw new Exception("Unable to parse");
-
-            return i;
+            identifier = $"{prefix}{word.AsString()}";
+            return result;
         }
 
-        public static ReadOnlySpan<char> TakeOne(
-            this ReadOnlySpan<char> input, Func<char, bool> predicate, out char match)
-        {
-            var i = MatchWhile(input, predicate, failOnMiss: true, take: 1);
-            match = input[0];
-
-            return input.Slice(i);
-        }
-
-        public static ReadOnlySpan<char> Take(
-            this ReadOnlySpan<char> input, Func<char, bool> predicate, out ReadOnlySpan<char> match)
-        {
-            var i = MatchWhile(input, predicate, failOnMiss: true);
-            match = input.Slice(0, i);
-
-            return input.Slice(i);
-        }
-
-        public static ReadOnlySpan<char> SkipOne(this ReadOnlySpan<char> input, Func<char, bool> predicate) =>
-            input.Slice(MatchWhile(input, predicate, failOnMiss: false, take: 1));
-
-        public static ReadOnlySpan<char> Skip(this ReadOnlySpan<char> input, Func<char, bool> predicate) =>
-            input.Slice(MatchWhile(input, predicate, failOnMiss: false));
-
-        public static string AsString(this ReadOnlySpan<char> input) =>
-            new String(input.ToArray());
-            
-        public static string Concat(this ReadOnlySpan<char> input, params ReadOnlySpan<char>[] rest)
-        {
-            var sb = new StringBuilder();
-
-            sb.Append(input.AsString());
-            for (var i = 0; i < rest.Length; ++i)
-                sb.Append(rest[i].AsString());
-
-            return sb.ToString();
-        }
-    }
-
-    class Program
-    {
         static void Main(string[] args)
         {
             var remaining = 
                 @"1  B3his is some string  "
                 .AsSpan()
-                .SkipOne(Char.IsDigit)
-                .Skip(Char.IsWhiteSpace)
-                .TakeOne(Char.IsLetter, out var prefix)
-                .Take(Char.IsLetterOrDigit, out var word)
-                .Skip(Char.IsWhiteSpace);
+                .SkipOne(IsDigit)
+                .Skip(IsWhiteSpace)
+                .ParseIdentifier(out var ident)
+                .Skip(IsWhiteSpace);
 
-            Console.WriteLine($"     Word: {prefix}{word.AsString()}");
+            Console.WriteLine($"    Ident: {ident}");
             Console.WriteLine($"Remaining: {remaining.AsString()}");
         }
     }
