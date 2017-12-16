4c96c75fe57c1e6ade097fb913635c7e8740a694
Erik O'Leary <erik.m.oleary@gmail.com>
2017-12-16 16:41:07 -0600

    Added doc comments

--- csparser/Parser.cs
+++ csparser/Parser.cs
@@ -1,13 +1,23 @@
 using System;
 using System.Runtime.CompilerServices;
-using System.Text;
 
 namespace csparser
 {
     public static partial class Parser
     {
-        public delegate bool Predicate(char id);
+        /// <summary>
+        /// Test if the input character matches
+        /// </summary>
+        /// <param name="c">Input character</param>
+        /// <returns>True if input char matches</returns>
+        public delegate bool Predicate(char c);
 
+        /// <summary>
+        /// Iterate through input until the predicate returns false
+        /// </summary>
+        /// <param name="input">Input to match</param>
+        /// <param name="predicate">Input test</param>
+        /// <returns>Index match end</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         static int MatchWhile(ref ReadOnlySpan<char> input, Predicate predicate)
         {
@@ -17,7 +27,15 @@ namespace csparser
 
             return i;
         }
-        
+
+        /// <summary>
+        /// Iterate through input until the predicate returns false or number of taken
+        /// characters taken is met
+        /// </summary>
+        /// <param name="input">Input to match</param>
+        /// <param name="predicate">Input test</param>
+        /// <param name="take">Number of characters to take</param>
+        /// <returns>Index match end</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         static int MatchWhile(ref ReadOnlySpan<char> input, Predicate predicate, int take)
         {
@@ -28,6 +46,13 @@ namespace csparser
             return i;
         }
 
+        /// <summary>
+        /// Take one character, if matching
+        /// </summary>
+        /// <param name="input">Input to match</param>
+        /// <param name="predicate">Input test</param>
+        /// <param name="match">Matching character</param>
+        /// <returns>Remainder of input</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ReadOnlySpan<char> TakeOne(
             this ReadOnlySpan<char> input, Predicate predicate, out char match)
@@ -38,6 +63,13 @@ namespace csparser
             return input.Slice(i);
         }
 
+        /// <summary>
+        /// Take multiple characters, while matching
+        /// </summary>
+        /// <param name="input">Input to match</param>
+        /// <param name="predicate">Input test</param>
+        /// <param name="match">Matching characters</param>
+        /// <returns>Remainder of input</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ReadOnlySpan<char> Take(
             this ReadOnlySpan<char> input, Predicate predicate, out ReadOnlySpan<char> match)
@@ -49,6 +81,22 @@ namespace csparser
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool Peek(this ReadOnlySpan<char> input, out char match)
+        {        
+            var result = Peek(input, 1, out var m);
+            match      = m[0];
+
+            return result;
+        }
+
+        /// <summary>
+        /// Peek multiple characters
+        /// </summary>
+        /// <param name="input">Input to match</param>
+        /// <param name="take">Input test</param>
+        /// <param name="match">Matching characters</param>
+        /// <returns>True if enough characters to match; otherwise false</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool Peek(this ReadOnlySpan<char> input, int take, out ReadOnlySpan<char> match)
         {
             if (input.Length < take)
@@ -59,23 +107,31 @@ namespace csparser
             return true;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool Peek(this ReadOnlySpan<char> input, out char match)
-        {        
-            var result = Peek(input, 1, out var m);
-            match      = m[0];
-
-            return result;
-        }
-
+        /// <summary>
+        /// Skip one character if predicate is true
+        /// </summary>
+        /// <param name="input">Input to match</param>
+        /// <param name="predicate">Input test</param>
+        /// <returns>Remainder of input</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ReadOnlySpan<char> SkipOne(this ReadOnlySpan<char> input, Predicate predicate) =>
             input.Slice(MatchWhile(ref input, predicate, take: 1));
 
+        /// <summary>
+        /// Skip while predicate is true
+        /// </summary>
+        /// <param name="input">Input to match</param>
+        /// <param name="predicate">Input test</param>
+        /// <returns>Remainder of input</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ReadOnlySpan<char> Skip(this ReadOnlySpan<char> input, Predicate predicate) =>
             input.Slice(MatchWhile(ref input, predicate));
 
+        /// <summary>
+        /// Convert input span to string
+        /// </summary>
+        /// <param name="input">Input span</param>
+        /// <returns>New string containing input characters</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static unsafe string AsString(this ReadOnlySpan<char> input)
         {
@@ -85,14 +141,32 @@ namespace csparser
             }
         }
             
+        /// <summary>
+        /// Concatentate two inputs
+        /// </summary>
+        /// <param name="input">Left-hand input</param>
+        /// <param name="rest">Right-hand input</param>
+        /// <returns>New string containing all input characters</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static string Concat(this ReadOnlySpan<char> input, ReadOnlySpan<char> rest) =>
             string.Concat(input.AsString(), rest.AsString());
 
+        /// <summary>
+        /// Assign and return remaining buffer
+        /// </summary>
+        /// <param name="rest">Remaining buffer</param>
+        /// <param name="id">Assignments</param>
+        /// <returns>Remaining buffer</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ReadOnlySpan<char> Let(this ReadOnlySpan<char> rest, string id) => 
             rest;
 
+        /// <summary>
+        /// Assign and return remaining buffer
+        /// </summary>
+        /// <param name="rest">Remaining buffer</param>
+        /// <param name="id">Assignments</param>
+        /// <returns>Remaining buffer</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ReadOnlySpan<char> Let<T>(this ReadOnlySpan<char> rest, T id) => 
             rest;
