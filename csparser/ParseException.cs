b28c348c759a74db9b2d7590ee85735b15d6f603
Erik O'Leary <erikoleary@Onionbook.local>
2017-12-21 14:30:19 -0600

    Message formatting

--- csparser/ParseException.cs
+++ csparser/ParseException.cs
@@ -16,19 +16,24 @@ namespace csparser
 
         public override string Message =>
             Context == null
-            ? $"{Context} ({Line},{Column}), Expected {Expected}, got: {Rest}..."
-            : $"({Line},{Column}), got: {Rest}...";
+                ? Expected == null
+                ? $"({Line},{Column}), got: {Rest}..."
+                : $"({Line},{Column}), Expected {Expected}, got: {Rest}..."
+                : Expected == null
+                ? $"{Context} ({Line},{Column}), got: {Rest}..."
+                : $"{Context} ({Line},{Column}), Expected {Expected}, got: {Rest}...";
 
-        public ParseException(int line, int col, string rest)
+        public ParseException(int line, int col, string rest, string expected = null)
         {
-            this.Line   = line;
-            this.Column = col;
-            this.Rest   = rest;
+            this.Line     = line;
+            this.Column   = col;
+            this.Rest     = rest;
+            this.Expected = expected;
         }
 
         public ParseException AddDetail(string file = null, string expected = null)
         {
-            this.Context     = file;
+            this.Context  = file;
             this.Expected = expected;
 
             return this;
