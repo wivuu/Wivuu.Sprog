dfff31dd5b799e40bdf57eb9a1f80d7392370c7a
Erik O'Leary <erik.m.oleary@gmail.com>
2023-11-22 09:39:07 -0600

    Improve performance of concat operation by leveraging unsafe

--- Sprog/SpanExtensions.cs
+++ Sprog/SpanExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace Wivuu.Sprog;
 
@@ -13,16 +14,22 @@ public static class SpanExtensions
     /// <param name="value">Input char</param>
     /// <returns>New string containing input characters</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static string Concat(
+    public static unsafe string Concat(
         this ReadOnlySpan<char> input,
         char value)
     {
-        Span<char> result = stackalloc char[input.Length + 1];
-
-        input.CopyTo(result);
-        result[^1] = value;
-
-        return new string(result);
+        fixed (char* f = &MemoryMarshal.GetReference(input))
+        {
+            return string.Create(
+                input.Length + 1,
+                (First: (IntPtr)f, FirstLength: input.Length, Second: value),
+                static (destination, state) =>
+                {
+                    new Span<char>((char*)state.First, state.FirstLength).CopyTo(destination);
+                    destination[^1] = state.Second;
+                }
+            );
+        }
     }
 
     /// <summary>
@@ -31,16 +38,22 @@ public static class SpanExtensions
     /// <param name="value">Input span</param>
     /// <returns>New string containing input characters</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static string Concat(
+    public static unsafe string Concat(
         this char input,
         ReadOnlySpan<char> value)
     {
-        Span<char> result = stackalloc char[value.Length + 1];
-
-        value.CopyTo(result[1..]);
-        result[0] = input;
-
-        return new string(result);
+        fixed (char* f = &MemoryMarshal.GetReference(value))
+        {
+            return string.Create(
+                value.Length + 1,
+                (First: input, Second: (IntPtr)f, SecondLength: value.Length),
+                static (destination, state) =>
+                {
+                    new Span<char>((char*)state.Second, state.SecondLength).CopyTo(destination[1..]);
+                    destination[0] = state.First;
+                }
+            );
+        }
     }
 
     /// <summary>
@@ -51,15 +64,8 @@ public static class SpanExtensions
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string Concat(
         this ReadOnlySpan<char> lhs,
-        ReadOnlySpan<char> rhs)
-    {
-        Span<char> result = stackalloc char[lhs.Length + rhs.Length];
-
-        lhs.CopyTo(result);
-        rhs.CopyTo(result[lhs.Length..]);
-
-        return new string(result);
-    }
+        ReadOnlySpan<char> rhs) => 
+        string.Concat(lhs, rhs);
 
     #endregion
 }
